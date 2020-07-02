bbbc71141b4a16d5a180b15dea131639aed7fd05
Misa-Kanuma <misa.kanuma@gmail.com>
2020-07-02 18:16:20 +0900

    [Fix]book_controller index,create修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,14 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new(book_params)
+    @user = User.find_by(id: current_user.id)
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user = current_user
+    @user = User.find_by(id: current_user.id)
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -41,7 +45,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
