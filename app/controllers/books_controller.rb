e3881df41365b75f170171ac2bed8e1f4305da22
Misa-Kanuma <misa.kanuma@gmail.com>
2020-07-02 23:53:26 +0900

    [Fix] edit if条件分岐修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,13 +25,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+      if @book.user != current_user
+         redirect_to books_path
+      end
   end
 
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
       render "edit"
     end
